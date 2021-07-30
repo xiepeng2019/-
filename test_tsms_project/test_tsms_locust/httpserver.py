dcf66eef7347d7e620570e8c2950190e26bc6514
404632795@qq.com <chuzhong1101>
2021-07-30 10:54:48 +0800

    修改文件

--- test_tsms_project/test_tsms_locust/httpserver.py
+++ test_tsms_project/test_tsms_locust/httpserver.py
@@ -11,28 +11,6 @@ from Config.db import test
 app = Flask(__name__)
 
 
-# database = DataBase(test.get("mongo_uri"))
-# db = database.connect_db(test.get("db"))
-#
-#
-# def insert_data_into_db(data):
-#     data.update(createAt=datetime.datetime.utcnow())
-#     try:
-#         db.sx_callback_data.insert_one(data)
-#     except Exception as e:
-#         database.close_db()
-#         raise Exception(f"fail to insert callback msg into db:{e}")
-#     finally:
-#         database.close_db()
-
-
-# @app.route('/callback', methods=['POST'])
-# def test_callback():
-#     data = json.loads(request.get_data(as_text=True))
-#     # insert_data_into_db(data)
-#     print(data)
-#     return "callback successfully"
-
 
 @app.route('/kronos', methods=['POST'])
 def kronos():

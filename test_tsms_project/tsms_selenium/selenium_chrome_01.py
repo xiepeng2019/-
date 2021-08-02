0f0f0a03b8056bb64918d4d6e9d6e81a8c98c88b
404632795@qq.com <chuzhong1101>
2021-08-02 11:49:35 +0800

    test

--- test_tsms_project/tsms_selenium/selenium_chrome_01.py
+++ test_tsms_project/tsms_selenium/selenium_chrome_01.py
@@ -11,26 +11,24 @@ from selenium.webdriver.support.wait import WebDriverWait
 import random
 import time
 
-
-b = webdriver.Chrome()
-b.get('https://www.baidu.com')
-time.sleep(5)
+browser = webdriver.Chrome()
+browser.get('https://www.baidu.com')
 #随机打开一个网页
-a = b.find_element_by_id('kw')
+a = browser.find_element_by_id('kw')
 a.send_keys('吃货')
 a.send_keys(Keys.ENTER)
 time.sleep(3)
-links=b.find_elements_by_css_selector("h3.t>a")
+links=browser.find_elements_by_css_selector("h3.t>a")
 links[random.randint(0,9)].click()
-# print(a)
-# print(b.current_url)
-#通过链接
-# input = b.find_element_by_link_text('秒杀').click()
-# print(b.current_url)
+print(a)
+print(browser.current_url)
+# # 通过链接
+# input = browser.find_element_by_link_text('秒杀').click()
+# print(browser.current_url)
 # print(input.text)
-
-# 通过classname 查找京东左边栏
-# c = b.find_elements_by_css_selector("ul li.service_item")
+#
+# # 通过classname 查找京东左边栏
+# c = browser.find_elements_by_css_selector("ul li.service_item")
 # for i in c:
 #         print(i.text)
 

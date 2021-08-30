15478e31a3e344ea3a5503a14695500182dc4e18
xiepeng.xz <xiepeng.xz@bytedance.com>
2021-08-30 15:44:40 +0800

    修改文件名

--- test_tsms_project/test_study_project/overall_review/python_base/any_func.py
+++ test_tsms_project/test_study_project/overall_review/python_base/any_func.py
@@ -1,3 +1,21 @@
+import random,string
+"""随机数练习"""
+def create_str(num_int=0,num_letters=0,num_zh=0,num_pun=0):
+    list = []
+    for i in range(num_int):
+        list.append(random.choice(string.digits))
+    for i in range(num_letters):
+        list.append(random.choice(string.ascii_letters))
+    for i in range(num_zh):
+        list.append(chr(random.randint(0x4e00,0x9fbf)))
+    for i in range(num_pun):
+        list.append(random.choice(string.punctuation))
+    random.shuffle(list)
+    return ''.join(list)
+# print(string.punctuation)
+a = create_str(1,1,1,1)
+print(a)
+
 
 
 n = 0
